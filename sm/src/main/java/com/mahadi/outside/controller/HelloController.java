fb8a66ecd3ede530d098712a37a009c9214825a0
mahadi-shakkor <shakkormahadi@gmail.com>
2026-07-30 02:22:39 +0600

    @PathVarible and many path in getmapping

--- sm/src/main/java/com/mahadi/outside/controller/HelloController.java
+++ sm/src/main/java/com/mahadi/outside/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.mahadi.outside.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/home")
-    public String sayHello(){
-        return "Hello";
+
+
+    @GetMapping({"/home/name/{name-id}/post/{post-id}","/home/name/{name-id}"})
+    public String sayHello(@PathVariable("name-id") Integer nameId, @PathVariable(value = "post-id" ,required = false) Integer postId) {
+        if (postId == null) {
+            return "Hello " + nameId;
+        }
+
+       return "Hello " + nameId + " " + postId;
     }
 }
