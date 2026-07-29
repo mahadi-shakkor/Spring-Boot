50edde48501ce91db1300a56171f1609177021a1
mahadi-shakkor <shakkormahadi@gmail.com>
2026-07-30 03:49:37 +0600

    @RequestMapping

--- sm/src/main/java/com/mahadi/outside/controller/HelloController.java
+++ sm/src/main/java/com/mahadi/outside/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.mahadi.outside.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -12,21 +10,23 @@ import static tools.jackson.databind.type.LogicalType.Map;
 public class HelloController {
 
 
-//    @GetMapping({"/dummy/users/{userid}/posts/{postid}","/api/dummy/users/{userid}"})
-//    public String sayHello(@PathVariable("userid") Integer nameId, @PathVariable(name = "postid" ,required = false) Integer postId) {
-//        if (postId == null) {
-//            return "Hello " + nameId;
-//        }
-//
-//       return "Hello " + nameId + " " + postId;
-//    }
-
-    @GetMapping({"/dummy/users/{userid}/posts/{postid}"})
-    public String sayHello(@PathVariable Map<String,String> j   ) {
-        if (j.get("postid") == null) {
-            return "Hello " + j.get("userid");
+    @RequestMapping(value = {"/dummy/users/{userid}/posts/{postid}"},method = {RequestMethod.GET,RequestMethod.POST})
+    public String sayHello(@PathVariable("userid") Integer nameId, @PathVariable(name = "postid" ,required = false) Integer postId) {
+        if (postId == null) {
+            return "Hello " + nameId;
         }
 
-        return "Hello " + j.get("userid") + " h  " + j.get("postid");
+       return "Hello " + nameId + " " + postId;
     }
+
+//    @GetMapping({"{userid}/posts/{postid}"})
+//    public String sayHello(@PathVariable Map<String,String> j   ) {
+//        if (j.get("postid") == null) {
+//            return "Hello " + j.get("userid");
+//        }
+//
+//        return "Hello " + j.get("userid") + " h  " + j.get("postid");
+//    }
+
+
 }
