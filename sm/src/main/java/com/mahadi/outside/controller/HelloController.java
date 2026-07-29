96b0166fb15e91682f3225f00fac646621581cef
mahadi-shakkor <shakkormahadi@gmail.com>
2026-07-29 13:16:14 +0600

    @ComponentScan usages in code

--- sm/src/main/java/com/mahadi/outside/controller/HelloController.java
+++ sm/src/main/java/com/mahadi/outside/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.mahadi.sm.controller;
+package com.mahadi.outside.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- sm/src/main/java/com/mahadi/sm/SmApplication.java
+++ sm/src/main/java/com/mahadi/sm/SmApplication.java
@@ -2,8 +2,10 @@ package com.mahadi.sm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.mahadi.outside.controller"})
 public class SmApplication {
 
 	public static void main(String[] args) {
