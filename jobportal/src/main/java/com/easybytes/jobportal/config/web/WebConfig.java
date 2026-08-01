fb0c1539f7a5ea01c40800db7fc7ad772d4c301f
mahadi-shakkor <shakkormahadi@gmail.com>
2026-08-01 07:40:29 +0600

    Docker volume  , docker run -p 3307:3306 --name=jobportaldb -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=jobportal -v C:/Users/mahad/Desktop/jobportal-data:/var/lib/mysql -d mysql

--- jobportal/src/main/java/com/easybytes/jobportal/config/web/WebConfig.java
+++ jobportal/src/main/java/com/easybytes/jobportal/config/web/WebConfig.java
@@ -4,7 +4,10 @@ import org.hibernate.annotations.ConcreteProxy;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
@@ -42,5 +45,30 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api",_->true);
+    }
+
+    /**
+     * Configure "global" cross-origin request processing. The configured CORS
+     * mappings apply to annotated controllers, functional endpoints, and static
+     * resources.
+     * <p>Annotated controllers can further declare more fine-grained config via
+     * {@link CrossOrigin @CrossOrigin}.
+     * In such cases "global" CORS configuration declared here is
+     * {@link CorsConfiguration#combine(CorsConfiguration) combined}
+     * with local CORS configuration defined on a controller method.
+     *
+     * @param registry
+     * @see CorsRegistry
+     * @see CorsConfiguration#combine(CorsConfiguration)
+     * @since 4.2
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+       registry.addMapping("/api/**")
+       .allowedOrigins("http://localhost:5173")
+               .allowedMethods("*")
+               .allowedHeaders("*")
+               .exposedHeaders("*")
+               .allowCredentials(true).maxAge(3600) ;
     }
 }
