7c2581aae96befe6c2b4aebbbc3005aa8ecc93c4
mahadi-shakkor <shakkormahadi@gmail.com>
2026-08-07 07:39:19 +0600

    show json like formated effor for every field by adding a custome methos in glovalException handler class - MethodArgumentNotValidException

--- jobportal/src/main/java/com/easybytes/jobportal/exception/GlobalExceptionHandler.java
+++ jobportal/src/main/java/com/easybytes/jobportal/exception/GlobalExceptionHandler.java
@@ -5,11 +5,16 @@ import com.easybytes.jobportal.dto.ErrorResponseDto;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,6 +34,14 @@ public class GlobalExceptionHandler {
                 webRequest.getDescription(false), HttpStatus.INTERNAL_SERVER_ERROR,
                 "A NullPointerException occurred due to : " + exception.getMessage(), LocalDateTime.now());
         return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        List<FieldError> fieldErrorList = exception.getBindingResult().getFieldErrors();
+        fieldErrorList.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
     }
 
 
